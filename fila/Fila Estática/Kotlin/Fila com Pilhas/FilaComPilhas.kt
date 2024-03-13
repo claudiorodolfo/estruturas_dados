bb8046295bfc6bc54718306aeb204fb83b4a2a2c
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-13 11:08:47 -0300

    Update FilaComPilhas.kt

--- fila/Fila Estática/Kotlin/Fila com Pilhas/FilaComPilhas.kt
+++ fila/Fila Estática/Kotlin/Fila com Pilhas/FilaComPilhas.kt
@@ -3,41 +3,40 @@ class FilaComPilhas(val tamanho: Int = 10) : Enfileiravel {
 	private var pilha1: Empilhavel = PilhaEstatica(tamanho) 
 	private var pilha2: Empilhavel = PilhaEstatica(tamanho)
 
-
-	override fun atualizar(dado: Any?) {
-        pilha2.atualizar(dado)
-    }
-
 	override fun enfileirar(dado: Any?) {
-        //coloca dado na pilha1
-        pilha1.empilhar(dado)
-        //volta dados da pilha2 para pilha1
-        while(!pilha2.estaVazia()) {
-            pilha1.empilhar(pilha2.desempilhar())
-        }
-        //volta todos os dados pra pilha2
+        //coloca dado na pilha2
+        pilha2.empilhar(dado)
+        //volta dados da pilha1 para pilha2
         while(!pilha1.estaVazia()) {
             pilha2.empilhar(pilha1.desempilhar())
+        }
+        //volta todos os dados pra pilha1
+        while(!pilha2.estaVazia()) {
+            pilha1.empilhar(pilha2.desempilhar())
         }        
     }
 	
+    override fun atualizar(dado: Any?) {
+        pilha1.atualizar(dado)
+    }
+
 	override fun desenfileirar(): Any? {
-        return pilha2.desempilhar()
+        return pilha1.desempilhar()
     }
 	
 	override fun frente(): Any? {
-        return pilha2.espiar()
+        return pilha1.espiar()
     }
 	
 	override fun estaCheia(): Boolean {
-        return pilha2.estaCheia()
+        return pilha1.estaCheia()
     }
 	
 	override fun estaVazia(): Boolean {
-        return pilha2.estaVazia()
+        return pilha1.estaVazia()
 	}
 	
 	override fun imprimir(): String {
-        return pilha2.imprimir()
+        return pilha1.imprimir()
     }
 }
