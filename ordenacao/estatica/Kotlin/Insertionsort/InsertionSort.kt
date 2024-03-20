90b89b95369d79aa6ae62feadd739b7384164153
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-20 12:37:16 -0300

    Update InsertionSort.kt
    Tornei o InsertionSort mais simples.

--- ordenacao/estatica/Kotlin/Insertionsort/InsertionSort.kt
+++ ordenacao/estatica/Kotlin/Insertionsort/InsertionSort.kt
@@ -18,13 +18,11 @@ class InsertionSort(private var dados: Array<Int>): Ordenavel {
     //sort
     override fun ordenar() {
         for (i in 1 until dados.size) {
-            val pivo =  dados[i]
-            var j = i - 1
-            while (j >= 0 && dados[j] > pivo) {
-                dados[j+1] = dados[j]
+            var j = i
+            while ((j > 0) && dados[j] < dados[j-1]) {
+                trocar(j, j-1)
                 j--
             }
-            dados[j+1] = pivo
         }
     }
 }
