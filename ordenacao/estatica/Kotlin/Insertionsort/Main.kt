0770245148e99a433cb6cfa97c939692fb0c0334
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-20 12:11:54 -0300

    Update Main.kt

--- ordenacao/estatica/Kotlin/Insertionsort/Main.kt
+++ ordenacao/estatica/Kotlin/Insertionsort/Main.kt
@@ -1,5 +1,9 @@
+import kotlin.random.Random
+
 fun main()  {
-    val dados = arrayOf(5, 7, 2, 6, 1, 4, 3, 0, 9, 8)
+    val random = Random
+    //10 números aleatórios entre 300 e 400
+    val dados = Array<Int>(10) { random.nextInt(300, 401) }
     var b = InsertionSort(dados)
     b.imprimir()
     b.ordenar()
