cb671457ffd6f80df18dac1948a0bc8fb0175054
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-03-20 13:47:43 -0300

    Update Main.kt

--- ordenacao/estatica/Kotlin/Insertionsort/Main.kt
+++ ordenacao/estatica/Kotlin/Insertionsort/Main.kt
@@ -1,21 +1,27 @@
 import kotlin.random.Random
-//import kotlin.system.measureTimeMillis
-//import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
+import kotlin.system.measureNanoTime
 
 fun main()  {
     val gerador = Random    
     //10 números aleatórios entre 300 e 400
     val dados = Array<Int>(10) { gerador.nextInt(300, 401) }
     var b = InsertionSort(dados)
+    //cópia de b
+    var b1 = b
 
     b.imprimir()
-    val tempoInicial = System.currentTimeMillis()
-    val tempoInicialNano = System.nanoTime()
-    b.ordenar()
-    val tempoFinalNano = System.nanoTime()
-    val tempoFinal = System.currentTimeMillis()
-    
+    val tempoNano = measureNanoTime {
+        b.ordenar()
+    }      
+    //neste momento b já está ordenado, 
+    //então tenho que ordenar novamente o array original, 
+    //por isso da necessidade de b1
+    val tempoMili = measureTimeMillis {
+        b1.ordenar()
+    }
+ 
     b.imprimir()
-    println("Tempo de execução: ${(tempoFinalNano - tempoInicialNano)} ns")
-    println("Tempo de execução: ${(tempoFinal - tempoInicial)} ms")
+    println("Tempo de execução: $tempoNano ns")
+    println("Tempo de execução: $tempoMili ms")
 }
