670266293a46e807f38a8fc56de88a2ac453b7d2
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-04-01 17:13:04 -0300

    Fila Dinâmica

---
+++ fila/Fila Dinâmica/Kotlin/Fila Dupla Terminação/EnfileiravelDupla.kt
@@ -0,0 +1,16 @@
+interface EnfileiravelDupla {
+    // Metodos principais
+    fun enfileirarInicio(dado: Any?)
+	fun enfileirarFim(dado: Any?) //mesmo que enfileirar da Fila Normal
+    fun desenfileirarInicio(): Any? //mesmo que desenfileirar da Fila Normal
+	fun desenfileirarFim(): Any?
+    fun atualizarInicio(dado: Any?)  
+    fun atualizarFim(dado: Any?)          
+    fun frente(): Any? //mesmo que frente da Fila Normal
+    fun tras(): Any?
+    // Metodos auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimirFrentePraTras(): String //mesmo que imprimir da Fila Normal
+    fun imprimirTrasPraFrente(): String
+}
---
+++ fila/Fila Dinâmica/Kotlin/Fila Dupla Terminação/Main.kt
@@ -0,0 +1,34 @@
+fun main() {
+	var fila: EnfileiravelDupla = FilaDinamicaDuplaTerminacao()
+	fila.enfileirarInicio("A")
+	println(fila.imprimirFrentePraTras()) //[A]
+	println(fila.tras()) //A
+	println(fila.frente()) //A	
+	fila.enfileirarInicio("B")
+	fila.desenfileirarFim()
+    fila.enfileirarFim("c")
+	fila.atualizarFim("C")
+	fila.desenfileirarInicio()
+	fila.enfileirarInicio("D") 
+	println(fila.imprimirFrentePraTras()) //[D,C]
+	fila.frente() 
+	fila.desenfileirarFim()
+	println(fila.tras()) //D    
+	fila.enfileirarInicio("e")
+	fila.atualizarInicio("E")    
+	println(fila.imprimirFrentePraTras())//[E,D]
+	fila.enfileirarFim("F")
+	fila.desenfileirarInicio()
+	fila.enfileirarFim("G")
+	println(fila.tras()) //G
+	fila.enfileirarInicio("H")
+	fila.desenfileirarFim()
+	fila.enfileirarFim("I")
+	println(fila.imprimirFrentePraTras()) //[H,D,F,I]
+	fila.enfileirarInicio("J")
+	fila.enfileirarFim("K")
+	fila.enfileirarFim("L")
+	println(fila.frente()) //J
+	fila.desenfileirarInicio()
+	println(fila.imprimirFrentePraTras()) //[H,D,F,I,K,L]
+}
