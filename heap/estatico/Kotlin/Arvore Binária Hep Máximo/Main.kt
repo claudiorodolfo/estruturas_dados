c407648c92406f08cfb312f5a1494da40119c1e0
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-04-02 21:56:57 -0300

    Update Main.kt

--- heap/estatico/Kotlin/Arvore Binária Hep Máximo/Main.kt
+++ heap/estatico/Kotlin/Arvore Binária Hep Máximo/Main.kt
@@ -6,9 +6,9 @@ fun main() {
   	heap.inserir(2)
 	heap.inserir(7)
 	heap.inserir(1)
-	println(heap.imprimir())//[8,,,,]
+	println(heap.imprimir())//[8,7,2,4,1]
 	println(heap.extrair())	//8
 	println(heap.obter())	//7
 	println(heap.obter())	//7
-	println(heap.imprimir()) //[7,,,]
+	println(heap.imprimir()) //[7,4,2,1]
 }
