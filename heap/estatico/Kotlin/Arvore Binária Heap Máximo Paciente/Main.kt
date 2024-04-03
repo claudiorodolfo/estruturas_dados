741cd098ce7701edb5f72de0a23c52c6d3c4256c
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-04-03 02:04:56 -0300

    Update Main.kt

--- heap/estatico/Kotlin/Arvore Binária Heap Máximo Paciente/Main.kt
+++ heap/estatico/Kotlin/Arvore Binária Heap Máximo Paciente/Main.kt
@@ -4,11 +4,11 @@ fun main() {
     heapPacientes.inserir(Paciente("Maria", 25, 1))
     heapPacientes.inserir(Paciente("Pedro", 40, 2))
     heapPacientes.inserir(Paciente("Ana", 35, 4))
-	println("")
-    println("Saída: ${heapPacientes.imprimir()}") // Saída: [Paciente(nome=Ana, idade=35, prioridade=4), Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Pedro, idade=40, prioridade=2), Paciente(nome=Maria, idade=25, prioridade=1)]
-	println("")
+    println("")
+    println("${heapPacientes.imprimir()}") // Saída: [Paciente(nome=Ana, idade=35, prioridade=4), Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Pedro, idade=40, prioridade=2), Paciente(nome=Maria, idade=25, prioridade=1)]
+    println("")
     val pacienteAtendido = heapPacientes.extrair()
     println("Paciente atendido: $pacienteAtendido") // Saída: Paciente atendido: Paciente(nome=Ana, idade=35, prioridade=4)
     println("")
-    println("Saída: ${heapPacientes.imprimir()}") // Saída: [Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Maria, idade=25, prioridade=1), Paciente(nome=Pedro, idade=40, prioridade=2)]
+    println("${heapPacientes.imprimir()}") // Saída: [Paciente(nome=João, idade=30, prioridade=3), Paciente(nome=Maria, idade=25, prioridade=1), Paciente(nome=Pedro, idade=40, prioridade=2)]
 }
