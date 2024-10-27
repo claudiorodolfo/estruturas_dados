54414a0e0aa9065340a14ed2172cce792e99578a
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2024-10-27 03:06:07 -0300

    Create SegundaChamada.kt

---
+++ lista/Lista Estática/Kotlin/Prova20242/SegundaChamada.kt
@@ -0,0 +1,54 @@
+//QUESTÃO 1
+fun occurrences(data: Any?): Array<Int> {
+	// Primeira passagem para contar o número de ocorrências
+	var qtdeOcorrencias = 0
+	for (i in 0 until quantidade) {
+		val aux = dados[(posicaoInicial+i)%dados.size]
+		if (aux == data) 
+			qtdeOcorrencias++
+	}
+	// Cria um array fixo para armazenar as posições das ocorrências
+	val arrayPosicoes = IntArray(qtdeOcorrencias) {0}
+	var posicao = 0
+	// Segunda passagem para armazenar as posições das ocorrências
+	for (i in 0 until quantidade) {
+		val aux = dados[(posicaoInicial+i)%dados.size]
+		if (aux == data) {
+			arrayPosicoes[posicao] = i
+			posicao++
+		}
+	}
+	return arrayPosicoes
+}
+
+//Questão 2
+fun decToHex(data: String): String {
+	var dividendo = data.toInt()
+	//coloca os restos da divisões sucessivas por 16, numa pilha
+	while (dividendo != 0) {
+		val resto = dividendo % 16
+		val quociente = dividendo / 16
+		dividendo = quociente
+		empilhar(resto)
+	}
+	
+	var resultado = ""
+	val DigHexadecimal = "0123456789ABCDEF"
+	//em vez de retornar o valor em decimal, retorna a letra correspondente
+	while(!estaVazia()) {
+		resultado += DigHexadecimal[desempilhar()]
+	}
+	return resultado
+}
+
+//Questão 3
+fun reverse(): Array<Int> {
+	for (i in 0 until quantidade/2) {
+		val indiceInicio = (ponteiroInicio+i)%dados.size
+		val indiceFim = ((ponteiroFim+dados.size)-i)%dados.size
+		//realiza a troca dos elementos
+		val temp = dados[indiceInicio]
+		dados[indiceInicio] = dados[indiceFim]
+		dados[indiceFim] = temp
+	}
+}
