c5181ceac4c41aceb083543a1e019bc9cf0928d3
Cláudio Rodolfo S. de Oliveira <31605720+claudiorodolfo@users.noreply.github.com>
2024-11-25 15:59:44 -0300

    Update EnfileiravelDupla.kt

--- fila/Fila Estática/Kotlin/Fila Dupla Terminação/EnfileiravelDupla.kt
+++ fila/Fila Estática/Kotlin/Fila Dupla Terminação/EnfileiravelDupla.kt
@@ -1,9 +1,9 @@
 interface EnfileiravelDupla {
     // Metodos principais
     fun enfileirarInicio(dado: Any?)
-	fun enfileirarFim(dado: Any?) //mesmo que enfileirar da Fila Normal
+    fun enfileirarFim(dado: Any?) //mesmo que enfileirar da Fila Normal
     fun desenfileirarInicio(): Any? //mesmo que desenfileirar da Fila Normal
-	fun desenfileirarFim(): Any?
+    fun desenfileirarFim(): Any?
     fun atualizarInicio(dado: Any?)  
     fun atualizarFim(dado: Any?)          
     fun frente(): Any? //mesmo que espiar da Fila Normal
