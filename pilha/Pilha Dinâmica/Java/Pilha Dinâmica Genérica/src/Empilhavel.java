701f925fb91cbb7c967555392a142d1b79a5b9b0
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2025-05-06 07:33:34 -0300

    DOC

--- pilha/Pilha Dinâmica/Java/Pilha Dinâmica Genérica/src/Empilhavel.java
+++ pilha/Pilha Dinâmica/Java/Pilha Dinâmica Genérica/src/Empilhavel.java
@@ -1,12 +1,31 @@
-//Executar todos os comandos dentro da pasta "Pilha Dinâmica Genérica"
+//Executar todos os comandos a partir da pasta "Pilha Dinâmica Genérica"
 
+//Para documentação:
 //javadoc -d doc src/*.java
 
-//compilar fonte: javac src/Empilhavel.java -d bin
+//compilar e executar o programa:
+//compilar um código-fonte específico: 
+//javac src/Empilhavel.java -d bin
 //ou
-//compilar fonte: javac src/*.java -d bin
+//compilar todos os arquivos-fontes: 
+//javac src/*.java -d bin
+//executar o programa: 
+//java -cp .;bin PilhaDinamicaGenericaMainCLI
 
-//executar fonte: java -cp .;bin PilhaDinamicaGenericaMainCLI
+//Para os testes unitários com junit:
+//compilar classe de teste: 
+//javac -cp .;bin;lib/junit-4.13.2.jar -d test test/PilhaDinamicaGenericaTest.java
+//executar testes: 
+//java -cp .;bin;test;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore PilhaDinamicaGenericaTest
+
+//Gerar binário
+//Gerar .jar
+//jar cvfe build/Main.jar PilhaDinamicaGenericaMainCLI -C bin .
+//Executar .jar
+//java -jar build/Main.jar
+
+//PS: para sistemas tipo Linux trocar ; ou : na separação dos itens do classpath
+
 
 /**
  * Esta interface indica as operações que uma classe que deseja 
