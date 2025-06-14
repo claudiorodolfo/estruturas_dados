cc7bdda1139c344e062af6fffb83ac87920c7e12
Cláudio Rodolfo Sousa de Oliveira <claudio.ifbavc@gmail.com>
2025-06-14 19:44:58 -0300

    Create Unidade1_2.java

---
+++ lista/Lista Estática/Java/20251/Unidade1_2.java
@@ -0,0 +1,115 @@
+//Questão 1
+public class VerificaBalanceamento {
+
+    public boolean estaBemFormado(String s) {
+        Empilhavel pilha = new PilhaEstatica(60);
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                pilha.empilhar(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (pilha.estaVazia()) return false;
+                char t = pilha.desempilhar();
+                if ((c == ')' && t != '(') ||
+                    (c == '}' && t != '{') ||
+                    (c == ']' && t != '['))
+                    return false;
+            }
+        }
+        return pilha.estaVazia();
+    }
+
+    public static void main(String[] args) {
+        String s1 = "{(()[({})])}";
+        String s2 = "([)]";
+        VerificaBalanceamento vb = new VerificaBalanceamento();
+        System.out.println(vb.estaBemFormado(s1)); // true
+        System.out.println(vb.estaBemFormado(s2)); // false
+    }
+}
+
+
+//Questão 2
+public class OperacoesConjunto {
+
+    // Método auxiliar para verificar se um elemento está presente em um array
+    private boolean contem(int[] array, int valor, int limite) {
+        for (int i = 0; i < limite; i++) {
+            if (array[i] == valor) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int[] copiarArray(int[] original, int tamanho) {
+        // Cria um novo array com elementos do array original, até o tamanho passado
+        int[] copia = new int[tamanho];
+
+        // Copia cada elemento do array original para o novo array
+        for (int i = 0; i < tamanho; i++) {
+            copia[i] = original[i];
+        }
+        return copia;
+    }
+
+    // Diferença: elementos de 'a' que não estão em 'b'
+    public int[] difference(int[] a, int[] b) {
+        int[] temp = new int[a.length];
+        int cont = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (!contem(b, a[i], b.length) && !contem(temp, a[i], cont)) {
+                temp[cont++] = a[i];
+            }
+        }
+
+        return copiarArray(temp, cont);
+    }
+
+    // União: todos os elementos de 'a' e 'b', sem duplicatas
+    public int[] union(int[] a, int[] b) {
+        int[] temp = new int[a.length + b.length];
+        int cont = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (!contem(temp, a[i], cont)) {
+                temp[cont++] = a[i];
+            }
+        }
+
+        for (int i = 0; i < b.length; i++) {
+            if (!contem(temp, b[i], cont)) {
+                temp[cont++] = b[i];
+            }
+        }
+
+        return copiarArray(temp, cont);
+    }
+
+    // Interseção: elementos comuns entre 'a' e 'b'
+    public int[] intersection(int[] a, int[] b) {
+        int[] temp = new int[Math.min(a.length, b.length)];
+        int cont = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (contem(b, a[i], b.length) && !contem(temp, a[i], cont)) {
+                temp[cont++] = a[i];
+            }
+        }
+
+        return copiarArray(temp, cont);
+    }
+
+    public static void main(String[] args) {
+        int[] lista1 = {1, 2, 3, 4};
+        int[] lista2 = {3, 4, 5, 6};
+
+        OperacoesListas ol = new OperacoesListas();
+        System.out.println("Diferença: ");
+        System.out.println(java.util.Arrays.toString(ol.difference(lista1, lista2)));
+        System.out.println("União: ");
+        System.out.println(java.util.Arrays.toString(ol.union(lista1, lista2)));
+        System.out.println("Interseção: ");
+        System.out.println(java.util.Arrays.toString(ol.intersection(lista1, lista2)));
+    }
+}
